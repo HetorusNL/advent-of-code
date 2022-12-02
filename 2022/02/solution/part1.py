1c3bd411e3d9bf387f221f0ae4734fedf2670c74
Tim Klein Nijenhuis <tim.klein.nijenhuis@demcon.nl>
2022-12-02 10:44:22 +0100

    added day 02 solution

--- 2022/02/solution/part1.py
+++ 2022/02/solution/part1.py
@@ -8,7 +8,52 @@ class Part1:
 
     def solve(self) -> None:
         print("solving...")
+        self.total_score = 0
+        for line in self.lines:
+            opponent, own = line.split(" ")
+            score = self.get_self_play_points(own) + self.get_match_points(own, opponent)
+            self.total_score += score
+
+    def get_match_points(self, own, opponent):
+        loss = 0
+        draw = 3
+        win = 6
+        match own:
+            case "X":
+                match opponent:
+                    case "A":
+                        return draw
+                    case "B":
+                        return loss
+                    case "C":
+                        return win
+            case "Y":
+                match opponent:
+                    case "A":
+                        return win
+                    case "B":
+                        return draw
+                    case "C":
+                        return loss
+            case "Z":
+                match opponent:
+                    case "A":
+                        return loss
+                    case "B":
+                        return win
+                    case "C":
+                        return draw
+        raise ValueError(f"invalid input own {own} and opponent {opponent}")
+
+    def get_self_play_points(self, own):
+        match own:
+            case "X":
+                return 1
+            case "Y":
+                return 2
+            case "Z":
+                return 3
+        raise ValueError(f"invalid input own {own}")
 
     def get_result(self) -> str:
-        result = 42
-        return f"the result of part 1 is: {result}"
+        return f"the score of the rock paper scissors playthrough is: {self.total_score}"
--- 2022/02/solution/part2.py
+++ 2022/02/solution/part2.py
@@ -8,7 +8,59 @@ class Part2:
 
     def solve(self) -> None:
         print("solving...")
+        self.total_score: int = 0
+        for line in self.lines:
+            opponent, target_match_result = line.split(" ")
+            own: str = self.get_own_play(target_match_result, opponent)
+            score: int = self.get_self_play_points(own) + self.get_match_points(target_match_result)
+            self.total_score += score
+
+    def get_match_points(self, target_match_result: str) -> int:
+        match target_match_result:
+            case "X":
+                return 0
+            case "Y":
+                return 3
+            case "Z":
+                return 6
+
+    def get_own_play(self, target_match_result: str, opponent: str) -> str:
+        match target_match_result:
+            case "X":
+                match opponent:
+                    case "A":
+                        return "Z"
+                    case "B":
+                        return "X"
+                    case "C":
+                        return "Y"
+            case "Y":
+                match opponent:
+                    case "A":
+                        return "X"
+                    case "B":
+                        return "Y"
+                    case "C":
+                        return "Z"
+            case "Z":
+                match opponent:
+                    case "A":
+                        return "Y"
+                    case "B":
+                        return "Z"
+                    case "C":
+                        return "X"
+        raise ValueError(f"invalid input target_match_result {target_match_result} and opponent {opponent}")
+
+    def get_self_play_points(self, own) -> int:
+        match own:
+            case "X":
+                return 1
+            case "Y":
+                return 2
+            case "Z":
+                return 3
+        raise ValueError(f"invalid input own {own}")
 
     def get_result(self) -> str:
-        result = 42
-        return f"the result of part 2 is: {result}"
+        return f"the score of the rock paper scissors playthrough is: {self.total_score}"
