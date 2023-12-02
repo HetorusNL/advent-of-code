08ebbadab81ce89e03e58f38a5148abfdb9e1e43
HetorusNL <tim@hetorus.nl>
2023-12-02 06:26:27 +0100

    added day 02 solution

--- 2023/02/solution/part1.py
+++ 2023/02/solution/part1.py
@@ -5,10 +5,26 @@ class Part1:
     def __init__(self, file: Path):
         with open(file) as f:
             self.lines = [line.strip() for line in f.readlines()]
+        self.cubes = {"red": 12, "green": 13, "blue": 14}
 
     def solve(self) -> None:
         print("solving...")
+        self.possible_games = 0
+        for line in self.lines:
+            game_id, games = line.split(":")
+            game_id = int(game_id.split(" ")[1].strip())
+            game_list = games.split(";")
+            if self.game_list_possible(game_list):
+                self.possible_games += game_id
+
+    def game_list_possible(self, game_list: list[str]) -> bool:
+        for game in game_list:
+            cubes_list = [cube.strip() for cube in game.split(",")]
+            for cube in cubes_list:
+                num_cubes, cube_type = cube.split(" ")
+                if int(num_cubes) > self.cubes[cube_type]:
+                    return False
+        return True
 
     def get_result(self) -> str:
-        result = 42
-        return f"the result of part 1 is: {result}"
+        return f"the result of part 1 is: {self.possible_games}"
--- 2023/02/solution/part2.py
+++ 2023/02/solution/part2.py
@@ -8,7 +8,24 @@ class Part2:
 
     def solve(self) -> None:
         print("solving...")
+        self.game_power = 0
+        for line in self.lines:
+            game_id, games = line.split(":")
+            game_id = int(game_id.split(" ")[1].strip())
+            game_list = games.split(";")
+            self.game_power += self.get_game_power(game_list)
+
+    def get_game_power(self, game_list: list[str]) -> int:
+        cubes_required = {"red": 0, "green": 0, "blue": 0}
+        for game in game_list:
+            cubes_list = [cube.strip() for cube in game.split(",")]
+            for cube in cubes_list:
+                num_cubes, cube_type = cube.split(" ")
+                cubes_required[cube_type] = max(cubes_required[cube_type], int(num_cubes))
+        game_power = 1
+        for cube_required in cubes_required.values():
+            game_power *= cube_required
+        return game_power
 
     def get_result(self) -> str:
-        result = 42
-        return f"the result of part 2 is: {result}"
+        return f"the result of part 2 is: {self.game_power}"
