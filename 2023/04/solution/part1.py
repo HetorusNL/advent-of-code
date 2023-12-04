34db812b3842b9faf850c6487180cedd14294259
HetorusNL <tim@hetorus.nl>
2023-12-04 05:28:13 +0100

    added day 04 solution

--- 2023/04/solution/part1.py
+++ 2023/04/solution/part1.py
@@ -8,7 +8,19 @@ class Part1:
 
     def solve(self) -> None:
         print("solving...")
+        self.score = 0
+        for line in self.lines:
+            line = line.split(":")[1].strip()
+            winning, your = line.split("|")
+            winning = [winning for winning in winning.split(" ") if winning]
+            your = [your for your in your.split(" ") if your]
+
+            good_numbers = 0
+            for number in your:
+                if number in winning:
+                    good_numbers += 1
+            if good_numbers:
+                self.score += 2 ** (good_numbers - 1)
 
     def get_result(self) -> str:
-        result = 42
-        return f"the result of part 1 is: {result}"
+        return f"the result of part 1 is: {self.score}"
--- 2023/04/solution/part2.py
+++ 2023/04/solution/part2.py
@@ -8,7 +8,20 @@ class Part2:
 
     def solve(self) -> None:
         print("solving...")
+        cards = {i: 1 for i in range(len(self.lines))}
+        for card_idx, line in enumerate(self.lines):
+            line = line.split(":")[1].strip()
+            winning, your = line.split("|")
+            winning = [winning for winning in winning.split(" ") if winning]
+            your = [your for your in your.split(" ") if your]
+
+            good_numbers = 0
+            for number in your:
+                if number in winning:
+                    good_numbers += 1
+            for i in range(card_idx + 1, card_idx + 1 + good_numbers):
+                cards[i] += cards[card_idx]
+        self.scratch_cards = sum(cards.values())
 
     def get_result(self) -> str:
-        result = 42
-        return f"the result of part 2 is: {result}"
+        return f"the result of part 2 is: {self.scratch_cards}"
