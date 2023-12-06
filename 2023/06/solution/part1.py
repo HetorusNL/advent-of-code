dc4a960edf73787adf2b9ed176fc0718066bc5e3
HetorusNL <tim@hetorus.nl>
2023-12-06 06:27:23 +0100

    added day 06 solution

--- 2023/06/solution/part1.py
+++ 2023/06/solution/part1.py
@@ -5,10 +5,23 @@ class Part1:
     def __init__(self, file: Path):
         with open(file) as f:
             self.lines = [line.strip() for line in f.readlines()]
+        self.times = list(map(int, [line for line in self.lines[0].split(" ")[1:] if line]))
+        self.distances = list(map(int, [line for line in self.lines[1].split(" ")[1:] if line]))
 
     def solve(self) -> None:
         print("solving...")
+        race_wins = {}
+        for race in range(len(self.times)):
+            race_wins[race] = 0
+            race_time = self.times[race]
+            race_distance = self.distances[race]
+            for charge_time in range(self.times[race]):
+                distance = (race_time - charge_time) * charge_time
+                if distance > race_distance:
+                    race_wins[race] += 1
+        self.result = 1
+        for num_race_wins in race_wins.values():
+            self.result *= num_race_wins
 
     def get_result(self) -> str:
-        result = 42
-        return f"the result of part 1 is: {result}"
+        return f"the result of part 1 is: {self.result}"
--- 2023/06/solution/part2.py
+++ 2023/06/solution/part2.py
@@ -5,10 +5,16 @@ class Part2:
     def __init__(self, file: Path):
         with open(file) as f:
             self.lines = [line.strip() for line in f.readlines()]
+        self.time = int("".join(line for line in self.lines[0].split(" ")[1:] if line))
+        self.distance = int("".join(line for line in self.lines[1].split(" ")[1:] if line))
 
     def solve(self) -> None:
         print("solving...")
+        self.result = 0
+        for charge_time in range(self.time):
+            distance = (self.time - charge_time) * charge_time
+            if distance > self.distance:
+                self.result += 1
 
     def get_result(self) -> str:
-        result = 42
-        return f"the result of part 2 is: {result}"
+        return f"the result of part 2 is: {self.result}"
