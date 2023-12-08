6e77ebe457f33512df82d5a32a19053ac4de42e7
HetorusNL <tim@hetorus.nl>
2023-12-08 06:51:37 +0100

    added day 08 solution

--- 2023/08/solution/part1.py
+++ 2023/08/solution/part1.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import re
 
 
 class Part1:
@@ -8,7 +9,23 @@ class Part1:
 
     def solve(self) -> None:
         print("solving...")
-        self.result = 42
+        self.instructions = [0 if char == "L" else 1 for char in self.lines[0]]
+        self._map = {}
+        for line in self.lines[2:]:
+            match = re.match(r"(?P<location>[A-Z]*) = \((?P<left>[A-Z]*), (?P<right>[A-Z]*)\)", line)
+            assert match
+            self._map[match["location"]] = [match["left"], match["right"]]
+        self.result = self.run_map()
+
+    def run_map(self):
+        steps = 0
+        location = "AAA"
+        while True:
+            for instruction in self.instructions:
+                location = self._map[location][instruction]
+                steps += 1
+            if location == "ZZZ":
+                return steps
 
     def get_result(self) -> str:
         return f"the result of part 1 is: {self.result}"
--- 2023/08/solution/part2.py
+++ 2023/08/solution/part2.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import re
+import math
 
 
 class Part2:
@@ -8,7 +10,27 @@ class Part2:
 
     def solve(self) -> None:
         print("solving...")
-        self.result = 42
+        self.instructions = [0 if char == "L" else 1 for char in self.lines[0]]
+        self._map: dict[str, list[str]] = {}
+        for line in self.lines[2:]:
+            match = re.match(r"(?P<location>[A-Z0-9]*) = \((?P<left>[A-Z0-9]*), (?P<right>[A-Z0-9]*)\)", line)
+            assert match
+            self._map[match["location"]] = [match["left"], match["right"]]
+        self.result = math.lcm(*self.run_map())
+
+    def run_map(self):
+        steps = 0
+        locations = list(filter(lambda location: location.endswith("A"), self._map.keys()))
+        cycle_length = [0] * len(locations)
+        while True:
+            for instruction in self.instructions:
+                steps += 1
+                for location_idx in range(len(locations)):
+                    locations[location_idx] = self._map[locations[location_idx]][instruction]
+                    if locations[location_idx].endswith("Z") and cycle_length[location_idx] == 0:
+                        cycle_length[location_idx] = steps
+                if 0 not in cycle_length:
+                    return cycle_length
 
     def get_result(self) -> str:
         return f"the result of part 2 is: {self.result}"
