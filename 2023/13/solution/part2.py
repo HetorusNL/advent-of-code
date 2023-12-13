cd989e24928d5013d89b0919973283fe6f4a20c2
HetorusNL <tim@hetorus.nl>
2023-12-13 07:07:46 +0100

    added day 13 solution

--- 2023/13/solution/part2.py
+++ 2023/13/solution/part2.py
@@ -5,10 +5,94 @@ class Part2:
     def __init__(self, file: Path):
         with open(file) as f:
             self.lines = [line.strip() for line in f.readlines()]
+        self.patterns: list[list[str]] = []
+        self.patterns.append([])
+        for line in self.lines:
+            if line:
+                self.patterns[-1].append(line)
+            else:
+                self.patterns.append([])
 
     def solve(self) -> None:
         print("solving...")
-        self.result = 42
+        self.result = 0
+        for pattern in self.patterns:
+            col_reflections, row_reflections = self.solve_pattern(pattern)
+            # unsmudge the pattern
+            smudge_found = False
+            for col_idx in range(len(pattern[0])):
+                for row_idx in range(len(pattern)):
+                    self.invert(pattern, col_idx, row_idx)
+                    new_col_reflections, new_row_reflections = self.solve_pattern(pattern)
+                    new_reflections = []
+                    for new_col_reflection in new_col_reflections:
+                        if new_col_reflection not in col_reflections:
+                            new_reflections.append(new_col_reflection)
+                    for new_row_reflection in new_row_reflections:
+                        if new_row_reflection not in row_reflections:
+                            new_reflections.append(new_row_reflection)
+                    if new_reflections:
+                        smudge_found = True
+                        self.result += sum(new_reflections)
+                        break
+                    # if not found, undo the inversion
+                    self.invert(pattern, col_idx, row_idx)
+                if smudge_found:
+                    break
+
+    def invert(self, pattern: list[str], col_idx: int, row_idx: int):
+        char = pattern[row_idx][col_idx]
+        new_char = "#" if char == "." else "."
+        row = pattern[row_idx]
+        start = row[:col_idx]
+        end = row[col_idx + 1 :]
+        new_row = start + new_char + end
+        pattern[row_idx] = new_row
+
+    def solve_pattern(self, pattern: list[str]):
+        col_reflections = []
+        row_reflections = []
+        # test column reflection
+        height = len(pattern)
+        width = len(pattern[0])
+        for col_idx in range(width - 1):
+            if all(pattern[row_idx][col_idx] == pattern[row_idx][col_idx + 1] for row_idx in range(height)):
+                # test if more to the top or bottom
+                is_reflection = True
+                if col_idx + 1 < width / 2:
+                    for test_col_idx in range(col_idx):
+                        if any(
+                            pattern[row_idx][test_col_idx] != pattern[row_idx][2 * col_idx + 1 - test_col_idx]
+                            for row_idx in range(height)
+                        ):
+                            is_reflection = False
+                else:
+                    for test_col_idx in range(col_idx, width):
+                        if any(
+                            pattern[row_idx][test_col_idx] != pattern[row_idx][col_idx - (test_col_idx - col_idx) + 1]
+                            for row_idx in range(height)
+                        ):
+                            is_reflection = False
+                if is_reflection:
+                    col_reflections.append(col_idx + 1)
+        # test row reflection
+        for row_idx in range(height - 1):
+            if pattern[row_idx] == pattern[row_idx + 1]:
+                # test if more to the right or left
+                is_reflection = True
+                if row_idx + 1 < height / 2:
+                    for test_row_idx in range(row_idx):
+                        if pattern[test_row_idx] != pattern[2 * row_idx + 1 - test_row_idx]:
+                            is_reflection = False
+                            break
+                else:
+                    for test_row_idx in range(row_idx + 1, height):
+                        if pattern[test_row_idx] != pattern[row_idx - (test_row_idx - row_idx) + 1]:
+                            is_reflection = False
+                            break
+                if is_reflection:
+                    row_reflections.append(100 * (row_idx + 1))
+        return col_reflections, row_reflections
 
     def get_result(self) -> str:
         return f"the result of part 2 is: {self.result}"
