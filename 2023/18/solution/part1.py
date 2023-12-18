5f9a585104886f063f1467f2a6b88cab7900fd72
HetorusNL <tim@hetorus.nl>
2023-12-18 08:30:28 +0100

    added day 18 solution

--- 2023/18/solution/part1.py
+++ 2023/18/solution/part1.py
@@ -8,7 +8,70 @@ class Part1:
 
     def solve(self) -> None:
         print("solving...")
-        self.result = 42
+        # initialize the grid
+        row = 0
+        col = 0
+        self.grid: dict[int, dict[int, bool]] = {}
+        self.grid[row] = {col: True}
+        # create the trench
+        for line in self.lines:
+            direction, num_meter, color = line.split(" ")
+            for _ in range(int(num_meter)):
+                match direction:
+                    case "U":
+                        row -= 1
+                    case "D":
+                        row += 1
+                    case "L":
+                        col -= 1
+                    case "R":
+                        col += 1
+                self.add_to_grid(row, col)
+
+        # do flood fill
+        lowest_row = min(self.grid.keys())
+        lowest_row_values = self.grid[lowest_row].keys()
+        center_col = abs(min(lowest_row_values) - max(lowest_row_values)) // 2
+        # find interior
+        row = lowest_row
+        col = center_col
+        found_border = False
+        while row in self.grid:
+            if found_border:
+                if not self.grid[row].get(col):
+                    break  # found the interior
+            else:
+                if self.grid[row].get(col):
+                    found_border = True
+            row += 1
+        # start flood fill
+        flood_pos = [[row, col]]
+        while flood_pos:
+            new_flood_pos = []
+            for row, col in flood_pos:
+                if not self.grid[row - 1].get(col):
+                    self.grid[row - 1][col] = True
+                    new_flood_pos.append([row - 1, col])
+                if not self.grid[row + 1].get(col):
+                    self.grid[row + 1][col] = True
+                    new_flood_pos.append([row + 1, col])
+                if not self.grid[row].get(col - 1):
+                    self.grid[row][col - 1] = True
+                    new_flood_pos.append([row, col - 1])
+                if not self.grid[row].get(col + 1):
+                    self.grid[row][col + 1] = True
+                    new_flood_pos.append([row, col + 1])
+            flood_pos = new_flood_pos
+
+        self.result = 0
+        for row in self.grid:
+            for col in self.grid[row]:
+                self.result += 1
+
+    def add_to_grid(self, row: int, col: int):
+        if row not in self.grid:
+            self.grid[row] = {}
+        self.grid[row][col] = True
 
     def get_result(self) -> str:
         return f"the result of part 1 is: {self.result}"
--- 2023/18/solution/part2.py
+++ 2023/18/solution/part2.py
@@ -8,7 +8,68 @@ class Part2:
 
     def solve(self) -> None:
         print("solving...")
-        self.result = 42
+        # initialize the grid
+        row = 0
+        col = 0
+        self.grid: dict[int, dict[int, bool]] = {}
+        self.grid[row] = {col: True}
+        # create the trench
+        for line in self.lines:
+            hex_code = line.split(" ")[-1][2:-1]
+            direction = {0: "R", 1: "D", 2: "L", 3: "U"}[int(hex_code[-1])]
+            num_meter = int(hex_code[:-1], 16)
+            print(direction, num_meter)
+            for _ in range(int(num_meter)):
+                match direction:
+                    case "U":
+                        row -= 1
+                    case "D":
+                        row += 1
+                    case "L":
+                        col -= 1
+                    case "R":
+                        col += 1
+                self.add_to_grid(row, col)
+
+        # do flood fill
+        self.result = 0
+        trench = 0
+        grid_length = len(self.grid)
+        chunk = grid_length // 100
+        current_percentage = 0
+        for row_idx, row in enumerate(self.grid):
+            if row_idx // chunk > current_percentage:
+                current_percentage = row_idx // chunk
+                print(f"processing rows: {current_percentage}%")
+            inside = False
+            entrance = ""
+            last_col = sorted(self.grid[row].keys())[0] - 2
+            for col in sorted(self.grid[row].keys()):
+                trench += 1
+                if col != last_col + 1:
+                    if inside:
+                        self.result += col - last_col - 1
+                if self.grid.get(row - 1, {}).get(col) and self.grid.get(row + 1, {}).get(col):
+                    inside = not inside
+                elif entrance == "D" and self.grid.get(row - 1, {}).get(col):
+                    inside = not inside
+                elif entrance == "U" and self.grid.get(row + 1, {}).get(col):
+                    inside = not inside
+                elif entrance == "U" and self.grid.get(row - 1, {}).get(col):
+                    entrance = ""
+                elif entrance == "D" and self.grid.get(row + 1, {}).get(col):
+                    entrance = ""
+                elif self.grid.get(row - 1, {}).get(col):
+                    entrance = "U"
+                elif self.grid.get(row + 1, {}).get(col):
+                    entrance = "D"
+                last_col = col
+        self.result += trench
+
+    def add_to_grid(self, row: int, col: int):
+        if row not in self.grid:
+            self.grid[row] = {}
+        self.grid[row][col] = True
 
     def get_result(self) -> str:
         return f"the result of part 2 is: {self.result}"
