4b792177ca9097edd5366b4b024d9615487caa66
HetorusNL <tim@hetorus.nl>
2024-12-02 06:53:08 +0100

    added day 02 solution

--- 2024/02/solution/part1.py
+++ 2024/02/solution/part1.py
@@ -8,7 +8,28 @@ class Part1:
 
     def solve(self) -> None:
         print("solving...")
-        self.result = 42
+        self.result = 0
+        for line in self.lines:
+            previous, *values = map(int, line.split())
+            fail = False
+            if values[0] > previous:
+                # all must be increasing
+                for value in values:
+                    if value - previous in [1, 2, 3]:
+                        previous = value
+                    else:
+                        fail = True
+                if not fail:
+                    self.result += 1
+            elif values[0] < previous:
+                # all must be decreasing
+                for value in values:
+                    if previous - value in [1, 2, 3]:
+                        previous = value
+                    else:
+                        fail = True
+                if not fail:
+                    self.result += 1
 
     def get_result(self) -> str:
         return f"the result of part 1 is: {self.result}"
--- 2024/02/solution/part2.py
+++ 2024/02/solution/part2.py
@@ -6,9 +6,46 @@ class Part2:
         with open(file) as f:
             self.lines = [line.strip() for line in f.readlines()]
 
+    def check_single(self, previous: int, values: list[int], func):
+        for value in values:
+            if func(value, previous) in [1, 2, 3]:
+                previous = value
+            else:
+                return False
+        # all elements processed successfully
+        self.result += 1
+        return True
+
+    def check_values(self, values: list[int]):
+        previous = values.pop(0)
+        # test if the sequence is increasing
+        func = lambda left, right: left - right
+        if self.check_single(previous, values, func):
+            return True
+        # test if the sequence is decreasing
+        func = lambda left, right: right - left
+        if self.check_single(previous, values, func):
+            return True
+        # otherwise the sequence is neither
+        return False
+
+    def report_checker(self, values: list[int]):
+        # check providing the array as is
+        if self.check_values(values.copy()):
+            return
+        # check with any elements removed as 'Problem Dampener'
+        for i in range(len(values)):
+            values_copy = values.copy()
+            values_copy.pop(i)
+            if self.check_values(values_copy.copy()):
+                return
+
     def solve(self) -> None:
         print("solving...")
-        self.result = 42
+        self.result = 0
+        for line in self.lines:
+            values = list(map(int, line.split()))
+            self.report_checker(values)
 
     def get_result(self) -> str:
         return f"the result of part 2 is: {self.result}"
