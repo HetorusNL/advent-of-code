f0066f79c91675282e5c5093859810679b1dd880
HetorusNL <tim@hetorus.nl>
2024-12-03 06:46:57 +0100

    added day 03 solution

--- 2024/03/solution/part1.py
+++ 2024/03/solution/part1.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import re
 
 
 class Part1:
@@ -8,7 +9,12 @@ class Part1:
 
     def solve(self) -> None:
         print("solving...")
-        self.result = 42
+        self.result = 0
+        for line in self.lines:
+            offset = 0
+            while match := re.search(r"mul\((?P<left>[0-9]*),(?P<right>[0-9]*)\)", line[offset:]):
+                offset += match.end()
+                self.result += int(match.groupdict()["left"]) * int(match.groupdict()["right"])
 
     def get_result(self) -> str:
         return f"the result of part 1 is: {self.result}"
--- 2024/03/solution/part2.py
+++ 2024/03/solution/part2.py
@@ -1,4 +1,12 @@
 from pathlib import Path
+import re
+
+
+class Result:
+    def __init__(self, offset: int, func: str, value: int | None = None):
+        self.offset: int = offset
+        self.func: str = func
+        self.value: int | None = value
 
 
 class Part2:
@@ -8,7 +16,42 @@ class Part2:
 
     def solve(self) -> None:
         print("solving...")
-        self.result = 42
+        self.result = 0
+        enabled = True
+        for line in self.lines:
+            offset = 0
+            while True:
+                results: list[Result] = []
+
+                # regex search for mul, add to results if found
+                if match := re.search(r"mul\((?P<left>[0-9]*),(?P<right>[0-9]*)\)", line[offset:]):
+                    value = int(match.groupdict()["left"]) * int(match.groupdict()["right"])
+                    results.append(Result(match.end(), "mul", value))
+                # regex search for don't, add to results if found
+                if match := re.search(r"don't\(\)", line[offset:]):
+                    results.append(Result(match.end(), "dont"))
+                # regex search for do, add to results if found
+                if match := re.search(r"do\(\)", line[offset:]):
+                    results.append(Result(match.end(), "do"))
+
+                # if we have results, process the one with the lowest offset
+                if results:
+                    result = sorted(results, key=lambda r: r.offset)[0]
+                    offset += result.offset
+                    match result.func:
+                        case "mul":
+                            assert result.value
+                            if enabled:
+                                self.result += result.value
+                        case "dont":
+                            enabled = False
+                        case "do":
+                            enabled = True
+                        case _:
+                            assert False
+                # otherwise, when no results, break from the loop
+                else:
+                    break
 
     def get_result(self) -> str:
         return f"the result of part 2 is: {self.result}"
