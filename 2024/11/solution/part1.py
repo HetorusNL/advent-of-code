d0bb315b4a5888dbd1cb42de0616d0c80b0ec5bf
HetorusNL <tim@hetorus.nl>
2024-12-11 08:21:36 +0100

    added day 11 solution

--- 2024/11/solution/part1.py
+++ 2024/11/solution/part1.py
@@ -5,10 +5,33 @@ class Part1:
     def __init__(self, file: Path):
         with open(file) as f:
             self.lines = [line.strip() for line in f.readlines()]
+        assert len(self.lines) == 1
+
+    def blink(self) -> None:
+        index: int = 0
+        while index < len(self.stones):
+            if self.stones[index] == 0:
+                # if 0, replace with 1
+                self.stones[index] = 1
+            elif len(str(self.stones[index])) % 2 == 0:
+                # if even num digits, split in 2 even sized new stones
+                length: int = len(str(self.stones[index])) // 2
+                first: str = str(self.stones[index])[:length]
+                second: str = str(self.stones[index])[length:]
+                self.stones[index] = int(first)
+                self.stones.insert(index + 1, int(second))
+                index += 1
+            else:
+                # multiply by 2024
+                self.stones[index] *= 2024
+            index += 1
 
     def solve(self) -> None:
         print("solving...")
-        self.result = 42
+        self.stones: list[int] = list(map(int, self.lines[0].split()))
+        for _ in range(25):
+            self.blink()
+        self.result = len(self.stones)
 
     def get_result(self) -> str:
         return f"the result of part 1 is: {self.result}"
--- 2024/11/solution/part2.py
+++ 2024/11/solution/part2.py
@@ -5,10 +5,44 @@ class Part2:
     def __init__(self, file: Path):
         with open(file) as f:
             self.lines = [line.strip() for line in f.readlines()]
+        assert len(self.lines) == 1
+
+    def blink(self, stone: int) -> list[int]:
+        if stone == 0:
+            # if 0, replace with 1
+            return [1]
+        elif len(str(stone)) % 2 == 0:
+            # if even num digits, split in 2 even sized new stones
+            length: int = len(str(stone)) // 2
+            first: str = str(stone)[:length]
+            second: str = str(stone)[length:]
+            return [int(first), int(second)]
+        else:
+            # multiply by 2024
+            return [stone * 2024]
+
+    def blink_dict(self, stones: dict[int, int]):
+        # store stone_value and count of stone values in a dict
+        new_stones: dict[int, int] = {}
+        for stone, num_stones in stones.items():
+            stones_after_blink = self.blink(stone)
+            for new_stone in stones_after_blink:
+                if new_stone not in new_stones:
+                    # add new entry in the stones dict with num_stones
+                    new_stones[new_stone] = num_stones
+                else:
+                    # add num_stones to the dict
+                    new_stones[new_stone] += num_stones
+        return new_stones
 
     def solve(self) -> None:
         print("solving...")
-        self.result = 42
+        self.result = 0
+        stones: list[int] = list(map(int, self.lines[0].split()))
+        stones_dict: dict[int, int] = {stone: 1 for stone in stones}
+        for _ in range(75):
+            stones_dict = self.blink_dict(stones_dict)
+        self.result = sum(stones_dict.values())
 
     def get_result(self) -> str:
         return f"the result of part 2 is: {self.result}"
